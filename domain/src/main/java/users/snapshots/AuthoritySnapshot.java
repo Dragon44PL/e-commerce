124ea176ec21a5cc8fb1317364100138c859e21b
Dragon44PL <krystian.pieniak44@gmail.com>
2021-04-01 14:03:46 +0200

    Change EntitySnapshot signature

--- domain/src/main/java/users/snapshots/AuthoritySnapshot.java
+++ domain/src/main/java/users/snapshots/AuthoritySnapshot.java
@@ -4,10 +4,4 @@ import domain.EntitySnapshot;
 
 import java.util.UUID;
 
-public record AuthoritySnapshot(UUID id, String name) implements EntitySnapshot<UUID> {
-
-    @Override
-    public UUID getId() {
-        return id;
-    }
-}
+public record AuthoritySnapshot(UUID id, String name) implements EntitySnapshot<UUID> { }
--- domain/src/main/java/users/snapshots/UserSnapshot.java
+++ domain/src/main/java/users/snapshots/UserSnapshot.java
@@ -5,10 +5,4 @@ import users.vo.Credentials;
 import java.util.Set;
 import java.util.UUID;
 
-public record UserSnapshot(UUID id, String mail, Credentials credentials, Set<RoleSnapshot> roles) implements EntitySnapshot<UUID> {
-
-    @Override
-    public UUID getId() {
-        return id;
-    }
-}
+public record UserSnapshot(UUID id, String mail, Credentials credentials, Set<RoleSnapshot> roles) implements EntitySnapshot<UUID> { }
