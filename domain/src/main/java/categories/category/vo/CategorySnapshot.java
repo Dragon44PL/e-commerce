d6076c3e45a5e15e31a09a7af522ca9d952e65cf
Dragon44PL <krystian.pieniak44@gmail.com>
2021-04-06 18:49:27 +0200

    Changed signature

--- domain/src/main/java/categories/category/vo/CategorySnapshot.java
+++ domain/src/main/java/categories/category/vo/CategorySnapshot.java
@@ -1,8 +1,8 @@
 package categories.category.vo;
 
 import categories.category.event.CategoryEvent;
-import domain.EntitySnapshot;
+import domain.vo.EventEntitySnapshot;
 import java.util.List;
 import java.util.UUID;
 
-public record CategorySnapshot(UUID id, String name, CategoryId topCategory, List<CategoryEvent> categoryEvents) implements EntitySnapshot { }
+public record CategorySnapshot(UUID id, String name, CategoryId parentCategory, List<CategoryEvent> events) implements EventEntitySnapshot<CategoryEvent> { }
