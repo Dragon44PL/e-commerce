ea2881f2beeda1ff4fc7d46e135d036a45ee376d
Dragon44PL <krystian.pieniak44@gmail.com>
2021-04-08 14:42:49 +0200

    Added fields from User class

--- domain/src/main/java/users/events/UserCreatedEvent.java
+++ domain/src/main/java/users/events/UserCreatedEvent.java
@@ -1,10 +1,13 @@
 package users.events;
 
+import users.vo.AccountId;
+import users.vo.UserInfo;
+
 import java.time.Instant;
 import java.util.UUID;
 
-public record UserCreatedEvent(Instant occurredOn, UUID aggregateId) implements UserEvent {
-    public UserCreatedEvent(UUID userId) {
-        this(Instant.now(), userId);
+public record UserCreatedEvent(Instant occurredOn, UUID aggregateId, AccountId account, UserInfo userInfo) implements UserEvent {
+    public UserCreatedEvent(UUID userId, AccountId accountId, UserInfo userInfo) {
+        this(Instant.now(), userId, accountId, userInfo);
     }
 }
