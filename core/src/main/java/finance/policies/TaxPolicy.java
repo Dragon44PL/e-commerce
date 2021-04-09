38e67a3227ccdf4c35634228870dfb244e9c117a
Dragon44PL <krystian.pieniak44@gmail.com>
2021-04-09 17:42:04 +0200

    Added possible exception to TaxPolicy signature

--- core/src/main/java/finance/policies/TaxPolicy.java
+++ core/src/main/java/finance/policies/TaxPolicy.java
@@ -1,8 +1,9 @@
 package finance.policies;
 
+import finance.exception.NegativeTaxRatioException;
 import finance.vo.Money;
 import finance.vo.Tax;
 
 public interface TaxPolicy {
-    Tax calculateTax(Money money);
+    Tax calculateTax(Money money) throws NegativeTaxRatioException;
 }
