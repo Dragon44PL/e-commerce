e8fbe08a880b776bc76143db75600ce392817def
Dragon44PL <krystian.pieniak44@gmail.com>
2021-04-22 14:59:48 +0200

    Added ProductAvailability field

--- domain/src/main/java/products/stock/event/ProductStockCreatedEvent.java
+++ domain/src/main/java/products/stock/event/ProductStockCreatedEvent.java
@@ -1,14 +1,15 @@
 package products.stock.event;
 
+import products.stock.vo.ProductAvailability;
 import products.stock.vo.ProductId;
 import products.stock.vo.ProductQuantity;
 import products.stock.vo.ProductState;
 import java.time.Instant;
 import java.util.UUID;
 
-public record ProductStockCreatedEvent(Instant occurredOn, UUID aggregateId, ProductId product, ProductState productState, ProductQuantity quantity) implements ProductStockEvent {
+public record ProductStockCreatedEvent(Instant occurredOn, UUID aggregateId, ProductId product, ProductAvailability productAvailability, ProductState productState, ProductQuantity quantity) implements ProductStockEvent {
 
-    public ProductStockCreatedEvent(UUID productStockId, ProductId product, ProductState productState, ProductQuantity quantity) {
-        this(Instant.now(), productStockId, product, productState, quantity);
+    public ProductStockCreatedEvent(UUID productStockId, ProductId product, ProductState productState, ProductAvailability productAvailability, ProductQuantity quantity) {
+        this(Instant.now(), productStockId, product, productAvailability, productState, quantity);
     }
 }
