d32e592d13e5a054bd7479896ee8310106aa0672
Dragon44PL <krystian.pieniak44@gmail.com>
2021-04-22 09:55:12 +0200

    Updated ProductAvailability

--- domain/src/main/java/products/stock/vo/ProductAvailability.java
+++ domain/src/main/java/products/stock/vo/ProductAvailability.java
@@ -1,11 +1,13 @@
 package products.stock.vo;
 
 public enum ProductAvailability {
+
     AVAILABLE, OUT_OF_STOCK;
 
     public boolean isAvailable() {
         return this == AVAILABLE;
     }
+
     public boolean isOutOfStock() {
         return this == OUT_OF_STOCK;
     }
