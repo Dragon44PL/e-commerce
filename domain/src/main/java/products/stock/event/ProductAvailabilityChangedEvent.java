7d602839cf53e502cb492ee1f4fc878601f89c9d
Dragon44PL <krystian.pieniak44@gmail.com>
2021-04-22 17:21:36 +0200

    Removed ProductState field

--- domain/src/main/java/products/stock/event/ProductAvailabilityChangedEvent.java
+++ domain/src/main/java/products/stock/event/ProductAvailabilityChangedEvent.java
@@ -1,13 +1,12 @@
 package products.stock.event;
 
 import products.stock.vo.ProductAvailability;
-import products.stock.vo.ProductState;
 import java.time.Instant;
 import java.util.UUID;
 
-public record ProductAvailabilityChangedEvent(Instant occurredOn, UUID aggregateId, ProductAvailability productAvailability, ProductState productState) implements ProductStockEvent {
+public record ProductAvailabilityChangedEvent(Instant occurredOn, UUID aggregateId, ProductAvailability productAvailability) implements ProductStockEvent {
 
-    public ProductAvailabilityChangedEvent(UUID productStockId, ProductAvailability productAvailability, ProductState productState) {
-        this(Instant.now(), productStockId, productAvailability, productState);
+    public ProductAvailabilityChangedEvent(UUID productStockId, ProductAvailability productAvailability) {
+        this(Instant.now(), productStockId, productAvailability);
     }
 }
