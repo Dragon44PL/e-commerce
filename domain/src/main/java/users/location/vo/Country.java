3f49127305fab7cab72d61cb42899357d807d6cd
Dragon44PL <krystian.pieniak44@gmail.com>
2021-05-06 11:06:51 +0200

    Changed Country Signature

--- domain/src/main/java/users/location/vo/Country.java
+++ domain/src/main/java/users/location/vo/Country.java
@@ -1,5 +1,11 @@
 package users.location.vo;
 
+import java.util.Currency;
 import java.util.Locale;
 
-public record Country(String name, String code, Locale locale) { }
+public record Country(Locale locale, Currency currency) {
+
+    public Country(Locale locale) {
+        this(locale, Currency.getInstance(locale));
+    }
+}
