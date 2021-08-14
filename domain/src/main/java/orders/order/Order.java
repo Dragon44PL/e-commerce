c4454e4962f56fa946490385721c264847e48200
Dragon44PL <krystian.pieniak44@gmail.com>
2021-08-14 10:49:45 +0200

    Added "restore()" and "create()" methods, added method "calculateTotalCost()". Updated OrderTest to test newly added methods

--- domain/src/main/java/orders/order/Order.java
+++ domain/src/main/java/orders/order/Order.java
@@ -1,16 +1,49 @@
 package orders.order;
 
 import domain.AggregateRoot;
-import orders.order.event.OrderEvent;
+import finance.vo.Money;
+import orders.order.event.*;
+import orders.order.vo.OrderDestination;
+import orders.order.vo.OrderStatus;
+import orders.order.vo.OrderedProduct;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 class Order extends AggregateRoot<UUID, OrderEvent> {
 
     private final UUID id;
+    private final Set<OrderedProduct> products;
+    private OrderStatus orderStatus;
+    private OrderDestination orderDestination;
 
-    Order(UUID id, List<OrderEvent> events) {
+    static Order create(UUID id, Set<OrderedProduct> products, OrderDestination orderDestination) {
+        final Order order = new Order(id, products, orderDestination, new ArrayList<>());
+        order.registerEvent(new OrderCreatedEvent(order.id, order.products, order.orderDestination, order.orderStatus));
+        return order;
+    }
+
+    static Order restore(UUID id, Set<OrderedProduct> products, OrderStatus orderStatus, OrderDestination orderDestination) {
+        return new Order(id, products, orderStatus, orderDestination, new ArrayList<>());
+    }
+
+    private Order(UUID id, Set<OrderedProduct> products, OrderDestination orderDestination, List<OrderEvent> events) {
+        this(id, products, OrderStatus.PENDING, orderDestination, events);
+    }
+
+    private Order(UUID id, Set<OrderedProduct> products, OrderStatus orderStatus, OrderDestination orderDestination, List<OrderEvent> events) {
         super(events);
         this.id = id;
+        this.products = products;
+        this.orderStatus = orderStatus;
+        this.orderDestination = orderDestination;
+    }
+
+    void calculateTotalCost() {
+        final Money totalCost = products.stream().map(OrderedProduct::calculateTotalPrice).reduce(Money.ZERO, Money::add);
+        final TotalPriceCalculatedEvent totalPriceCalculatedEvent = new TotalPriceCalculatedEvent(id, totalCost);
+        this.registerEvent(totalPriceCalculatedEvent);
     }
 }
--- domain/src/test/java/orders/order/OrderTest.java
+++ domain/src/test/java/orders/order/OrderTest.java
@@ -1,4 +1,107 @@
 package orders.order;
 
+import finance.vo.Money;
+import orders.order.event.OrderCreatedEvent;
+import orders.order.event.OrderEvent;
+import orders.order.event.TotalPriceCalculatedEvent;
+import orders.order.vo.OrderDestination;
+import orders.order.vo.OrderStatus;
+import orders.order.vo.OrderedProduct;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 class OrderTest {
+
+    private final UUID ORDER_ID = UUID.randomUUID();
+    private final OrderedProduct ORDERED_PRODUCT = OrderedProduct.create(UUID.randomUUID(), new Money(5, Money.DEFAULT_CURRENCY));
+    private final OrderDestination ORDER_DESTINATION = new OrderDestination(UUID.randomUUID());
+    private final OrderStatus ORDER_STATUS = OrderStatus.PENDING;
+
+    // OrderEvents
+    private final Class<OrderCreatedEvent> ORDER_CREATED_EVENT = OrderCreatedEvent.class;
+    private final Class<TotalPriceCalculatedEvent> TOTAL_PRICE_CALCULATED_EVENT = TotalPriceCalculatedEvent.class;
+
+    @Test
+    @DisplayName("Order Creation Should Generate OrderCreatedEvent properly")
+    void orderCreationShouldCreateProperly() {
+        final Set<OrderedProduct> orderedProducts = Set.of(ORDERED_PRODUCT);
+        final Order order = Order.create(ORDER_ID, orderedProducts, ORDER_DESTINATION);
+
+        final Optional<OrderEvent> orderEvent = order.findLatestEvent();
+        assertTrue(orderEvent.isPresent());
+        assertEquals(ORDER_CREATED_EVENT, orderEvent.get().getClass());
+
+        final OrderCreatedEvent orderCreatedEvent = (OrderCreatedEvent) orderEvent.get();
+        assertEquals(orderCreatedEvent.aggregateId(), ORDER_ID);
+        assertEquals(orderCreatedEvent.products(), orderedProducts);
+        assertEquals(orderCreatedEvent.destination(), ORDER_DESTINATION);
+        assertEquals(orderCreatedEvent.orderStatus(), OrderStatus.PENDING);
+    }
+
+    @Test
+    @DisplayName("Order Restore Should Not Generate ProductCreatedEvent")
+    void orderRestoreShouldNotGenerateEvent() {
+        final Set<OrderedProduct> orderedProducts = Set.of(ORDERED_PRODUCT);
+        final Order order = Order.restore(ORDER_ID, orderedProducts, ORDER_STATUS, ORDER_DESTINATION);
+
+        final Optional<OrderEvent> orderEvent = order.findLatestEvent();
+        assertFalse(orderEvent.isPresent());
+    }
+
+    @Test
+    @DisplayName("Order Should Calculate Total Cost Correctly When One Product")
+    void orderShouldCalculateTotalCostCorrectlyWhenOneProduct() {
+        final Set<OrderedProduct> orderedProducts = Set.of(ORDERED_PRODUCT);
+        final Order order = Order.restore(ORDER_ID, orderedProducts, ORDER_STATUS, ORDER_DESTINATION);
+        order.calculateTotalCost();
+
+        final Optional<OrderEvent> orderEvent = order.findLatestEvent();
+        assertTrue(orderEvent.isPresent());
+        assertEquals(TOTAL_PRICE_CALCULATED_EVENT, orderEvent.get().getClass());
+
+        final TotalPriceCalculatedEvent totalPriceCalculatedEvent = (TotalPriceCalculatedEvent) orderEvent.get();
+        assertEquals(totalPriceCalculatedEvent.aggregateId(), ORDER_ID);
+        assertEquals(totalPriceCalculatedEvent.price(), ORDERED_PRODUCT.calculateTotalPrice());
+    }
+
+    @Test
+    @DisplayName("Order Should Calculate Total Cost Correctly When One Product")
+    void orderShouldCalculateTotalCostCorrectlyWhenMultipleProduct() {
+        final OrderedProduct firstProduct = new OrderedProduct(UUID.randomUUID(), new Money(7, Money.DEFAULT_CURRENCY), 5);
+        final OrderedProduct secondProduct = new OrderedProduct(UUID.randomUUID(), new Money(10, Money.DEFAULT_CURRENCY), 13);
+
+        final Set<OrderedProduct> orderedProducts = Set.of(firstProduct, secondProduct);
+        final Order order = Order.restore(ORDER_ID, orderedProducts, ORDER_STATUS, ORDER_DESTINATION);
+        order.calculateTotalCost();
+
+        final Optional<OrderEvent> orderEvent = order.findLatestEvent();
+        assertTrue(orderEvent.isPresent());
+        assertEquals(TOTAL_PRICE_CALCULATED_EVENT, orderEvent.get().getClass());
+
+        final TotalPriceCalculatedEvent totalPriceCalculatedEvent = (TotalPriceCalculatedEvent) orderEvent.get();
+        assertEquals(totalPriceCalculatedEvent.aggregateId(), ORDER_ID);
+        assertEquals(totalPriceCalculatedEvent.price(), firstProduct.calculateTotalPrice().add(secondProduct.calculateTotalPrice()));
+    }
+
+    @Test
+    @DisplayName("Order Should Calculate Zero When No Product")
+    void orderShouldCalculateZeroWhenNoProduct() {
+        final Order order = Order.restore(ORDER_ID, new HashSet<>(), ORDER_STATUS, ORDER_DESTINATION);
+        order.calculateTotalCost();
+
+        final Optional<OrderEvent> orderEvent = order.findLatestEvent();
+        assertTrue(orderEvent.isPresent());
+        assertEquals(TOTAL_PRICE_CALCULATED_EVENT, orderEvent.get().getClass());
+
+        final TotalPriceCalculatedEvent totalPriceCalculatedEvent = (TotalPriceCalculatedEvent) orderEvent.get();
+        assertEquals(totalPriceCalculatedEvent.aggregateId(), ORDER_ID);
+        assertEquals(totalPriceCalculatedEvent.price(), Money.ZERO);
+    }
 }
