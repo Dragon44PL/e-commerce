5df3403f313207c6c18e7a72632df292df9bcafb
Dragon44PL <krystian.pieniak44@gmail.com>
2021-09-07 20:01:40 +0200

    Changed "deleteById" to "delete", added "findById" method

--- core/src/main/java/org/ecommerce/shop/core/DomainRepository.java
+++ core/src/main/java/org/ecommerce/shop/core/DomainRepository.java
@@ -1,6 +1,7 @@
 package org.ecommerce.shop.core;
 
 public interface DomainRepository<I, T extends Aggregate> {
-    void deleteById(I id);
+    void findById(I id);
+    void delete(T t);
     T save(T t);
 }
